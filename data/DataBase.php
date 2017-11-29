2d6276c586c7848fd138d98f3520969cf8fb93c5
shaolizhi1234 <shaolizhi1234@126.com>
2017-11-29 11:11:20 +0800

    更新DataBase文件，内含数据库信息

---
+++ data/DataBase.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shaol
+ * Date: 2017/11/29
+ * Time: 11:09
+ */
+//Four components of MYSQL_DSN
+const MYSQL_HOST = "localhost";
+const MYSQL_DBNAME = "sunshine_box";
+const MYSQL_PORT = "3306";
+const MYSQL_CHARSET = "utf8";
+
+//Three afferent parameters of PDO
+const MYSQL_DSN = "mysql:host=" . MYSQL_HOST . ";port=" . MYSQL_PORT . ";dbname=" . MYSQL_DBNAME . ";charset=" . MYSQL_CHARSET;
+const MYSQL_USERNAME = "root";
+const MYSQL_PASSWORD = "Sheldon950518$";
