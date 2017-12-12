03432cdfd495cad4b0fa4aae1fff6d405245456a
shaolizhi1234 <shaolizhi1234@126.com>
2017-12-12 14:46:33 +0800

    退出激活完成

---
+++ activation_system/ExitActivated.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shaol
+ * Date: 2017/12/8
+ * Time: 16:54
+ */
+
+require_once dirname(__DIR__) . "/utils/RequireUtil.php";
+ini_set("display_errors", "on");
+
+$response_data = entrance();
+header('Content-Type: application/json');
+print json_encode($response_data);
+
+function entrance(): array
+{
+    //传入参数不正确，返回402
+    if (!isset($_POST['uuid'])) {
+        return returnJson('402');
+    }
+
+    //如果数据库连接失败，返回401
+    try {
+        $db = tryToConnectToTheDatabase();
+    } catch (PDOException $exception) {
+        return returnJson('401');
+    }
+
+    $uuid = $_POST['uuid'];
+
+    try {
+        $stmt = $db->prepare("UPDATE users SET is_activated=\"false\" WHERE uuid LIKE ?");
+        $stmt->execute([(string)$uuid]);
+        $count = $stmt->rowCount();
+    } catch (PDOException $exception) {
+        return returnJson('401');
+    }
+
+    if ($count == 1) {
+        return returnJson("001", "success");
+    } else {
+        return returnJson("001", "failure");
+    }
+}
