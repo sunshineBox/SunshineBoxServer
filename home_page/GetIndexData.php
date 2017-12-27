e1c040b43e6e03f009879e1bb6bba8cff40fc5dd
shaolizhi1234 <shaolizhi1234@126.com>
2017-12-27 12:00:51 +0800

    增量更新：给GetIndexData.php新添了一个输入参数:max_last_modification_time，这标志着客户端上的最后改动时间

--- home_page/GetIndexData.php
+++ home_page/GetIndexData.php
@@ -19,6 +19,10 @@ function entrance(): array
         return returnJson('402');
     }
 
+    if (!isset($_POST['max_last_modification_time'])) {
+        return returnJson('402');
+    }
+
     //如果数据库连接失败，返回401
     try {
         $db = tryToConnectToTheDatabase();
@@ -26,36 +30,41 @@ function entrance(): array
         return returnJson('401');
     }
 
+    //获取传入参数
     $course_type = $_POST['course_type'];
+    $max_last_modification_time = $_POST['max_last_modification_time'];
+
+    //查询数据库
     try {
-        $stmt = sqlQuery($db, $course_type);
+        $stmt = sqlQuery($db, $course_type, $max_last_modification_time);
     } catch (PDOException $exception) {
         return returnJson('401');
     } catch (Exception $exception) {
         return returnJson('001', 'failure');
     }
 
+    //返回JSON
     return returnJson('001', 'success', $stmt->fetchAll());
 }
 
-function sqlQuery(PDO $db, string $course_type): PDOStatement
+function sqlQuery(PDO $db, string $course_type, string $max_last_modification_time): PDOStatement
 {
     switch ($course_type) {
         case "music":
-            $stmt = $db->prepare("SELECT * FROM music_view");
-            $stmt->execute();
+            $stmt = $db->prepare("SELECT * FROM music_view WHERE last_modification_time > (?)");
+            $stmt->execute([$max_last_modification_time]);
             return $stmt;
         case "rhymes":
-            $stmt = $db->prepare("SELECT * FROM rhymes_view");
-            $stmt->execute();
+            $stmt = $db->prepare("SELECT * FROM rhymes_view WHERE last_modification_time > (?)");
+            $stmt->execute([$max_last_modification_time]);
             return $stmt;
         case "game":
-            $stmt = $db->prepare("SELECT * FROM game_view");
-            $stmt->execute();
+            $stmt = $db->prepare("SELECT * FROM game_view WHERE last_modification_time > (?)");
+            $stmt->execute([$max_last_modification_time]);
             return $stmt;
         case "reading":
-            $stmt = $db->prepare("SELECT * FROM reading_view");
-            $stmt->execute();
+            $stmt = $db->prepare("SELECT * FROM reading_view WHERE last_modification_time > (?)");
+            $stmt->execute([$max_last_modification_time]);
             return $stmt;
         default:
             throw new Exception('There is a problem with the value of $course_type');
